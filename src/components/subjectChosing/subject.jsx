974b14dfd9823d93636af1152011adc0cb920982
KalpeAslan <79480776+KalpeAslan@users.noreply.github.com>
2021-07-11 22:13:38 +0600

    fix: adadtive for subjects choosing modal perf: subjects fix: areaChosing import modalFade

--- src/components/subjectChosing/subject.jsx
+++ src/components/subjectChosing/subject.jsx
@@ -1,15 +1,13 @@
 import React, {useEffect, useState} from "react"
 import SubjectItem from './subjectItem.jsx'
-import {List, makeStyles, Box, Typography, IconButton} from "@material-ui/core"
+import {List, makeStyles, Box, Typography, IconButton, useMediaQuery, useTheme} from "@material-ui/core"
 import ModalFade from 'cpm/modalFade'
 import {useDispatch, useSelector} from "react-redux"
 import subjectsAll from 'services/calc/subjectsAll'
 import {ArrowBackIos} from "@material-ui/icons"
 
 const allSubjects = subjectsAll('ru')
-const modalWidth = 500
-const md = 1000
-
+const maxWidthVw = '80vw'
 const useStyles = makeStyles((theme) => ({
     modal: {
         display: 'flex',
@@ -17,29 +15,19 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: 'center',
     },
     wrapper: {
-        width: modalWidth,
-        [theme.breakpoints.down(md)]: {
-            width: '80vw',
+        width: 500,
+        [theme.breakpoints.down('md')]: {
+            width: maxWidthVw,
         },
         overflowX: 'hidden',
         backgroundColor: theme.palette.background.paper,
         border: '2px solid #000',
         boxShadow: theme.shadows[5],
-        '*': {
-            transform: `translateX(-${modalWidth}px)`,
-        }
-    },
-    listWrapper: {
-        transition: 'transform 0.4s',
-        transform: `translateX(-${modalWidth}px)`,
-        [theme.breakpoints.down(md)]: {
-            transform: 'translateX(-80vw)',
-        },
     },
     list: {
-        width: modalWidth,
-        [theme.breakpoints.down(md)]: {
-            width: '80vw',
+        width: 500,
+        [theme.breakpoints.down('md')]: {
+            width: maxWidthVw,
         },
         overflowY: 'auto',
         maxHeight: '80vh',
@@ -72,6 +60,8 @@ export default function ({openModal, setOpenModal}) {
     const [secondSubjects, setSecondSubject] = useState([])
     const firstSubject = useSelector(state => state.calc.firstSubject)
     const secondSubject = useSelector(state => state.calc.secondSubject)
+    const theme = useTheme()
+    const isMd = useMediaQuery(theme.breakpoints.down('md'))
     const [translateX, setTranslateX] = useState(0)
     const selectedSubjectIndexToChange = useSelector(state => state.calc.selectedSubjectIndexToChange)
 
@@ -84,13 +74,14 @@ export default function ({openModal, setOpenModal}) {
     }, [openModal])
 
     const clickFirstSubject = (subject) => {
-        setTranslateX(500)
         const combinedSubjects = getCombinedSubjects(subject)
         setSecondSubject(getCombinedSubjects(subject))
         dispatch({type: 'firstSubject', value: subject})
         if (secondSubject !== null && !combinedSubjects.includes(secondSubject)) {
             dispatch({type: 'secondSubject', value: null})
         }
+        if (isMd) return setTranslateX(maxWidthVw)
+        setTranslateX(500)
     }
 
     const clickSecondSubject = (subject) => {
@@ -105,7 +96,8 @@ export default function ({openModal, setOpenModal}) {
 
     return <ModalFade setOpenModal={setOpenModal} openModal={openModal}>
         <Box display="flex" className={classes.wrapper}>
-            <Box display="flex" flexDirection="column"  style={{transition: 'transform 0.4s',transform: `translateX(-${translateX}px)`}}>
+            <Box display="flex" flexDirection="column"
+                 style={{transition: 'transform 0.4s', transform: `translateX(-${translateX}px)`}}>
                 <Typography variant="h5" className={classes.modalTitle}>
                     Выбери первый предмет
                 </Typography>
@@ -116,7 +108,8 @@ export default function ({openModal, setOpenModal}) {
                     })}
                 </List>
             </Box>
-            <Box display="flex" flexDirection="column" style={{transition: 'transform 0.4s', transform: `translateX(-${translateX}px)`}}>
+            <Box display="flex" flexDirection="column"
+                 style={{transition: 'transform 0.4s', transform: `translateX(-${translateX}px)`}}>
                 <Box>
                     <IconButton onClick={clickBackButton} style={{float: 'left'}}>
                         <ArrowBackIos/>
